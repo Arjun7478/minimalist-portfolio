fa46238b29669275ff5865196269e883884458a3
arjunbarman74 <abcob97@gmail.com>
2024-02-18 21:44:45 +0530

    1st cmt

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "Slightly-Desaturated-Cyan": "#5FB4A2",
+        "Dark-Blue": "#203A4C",
+        "Grayish-Dark-Blue": "#33323D",
+        "Very-Light-Grey": "#FAFAFA",
+        "Light-Grey": " #EAEAEB",
+        "Bright Red": " #F43030",
+      },
+    },
+  },
+  plugins: [],
+};
