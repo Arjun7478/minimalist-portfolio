e1172c386bcef6665017fddb21e1583e52266879
arjunbarman74 <abcob97@gmail.com>
2024-02-23 11:30:47 +0530

    html indent fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        "slightly-desaturated-cyan": "#5FB4A2",
-        "dark-blue": "#203A4C",
-        "grayish-dark-blue": "#33323D",
-        "very-light-grey": "#FAFAFA",
-        "light-grey": " #EAEAEB",
-        "bright-red": " #F43030",
-      },
+    content: ["*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "slightly-desaturated-cyan": "#5FB4A2",
+                "dark-blue": "#203A4C",
+                "grayish-dark-blue": "#33323D",
+                "very-light-grey": "#FAFAFA",
+                "light-grey": "#EAEAEB",
+                "bright-red": "#F43030",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
